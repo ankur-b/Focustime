a5c60e912fe171a262f01c5d26a14f6fb6de392e
ankur-b <barveankur29@outlook.com>
2021-07-22 15:34:54 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+        <View>
+        <Text>Hello World</Text>
+        </View>
+      </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
